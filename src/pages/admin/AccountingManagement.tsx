e28ac3f2ec4c503864aefee5c4e55fd886ffb338
giuseppe1985-gt <gt.gestionale@gmail.com>
2026-01-16 19:31:53 +0100

    Update AccountingManagement.tsx

--- src/pages/admin/AccountingManagement.tsx
+++ src/pages/admin/AccountingManagement.tsx
@@ -1486,6 +1486,7 @@ setWorksiteExpenseInvoices(wsExpensesRes.data || []);
     </div>
   </div>
 )}
+          </div>
 
       {showInvoiceModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
